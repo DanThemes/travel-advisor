dde95a2b2b47138e58fe10664d268906ad2c7e04
Danthemes <danthemes@gmail.com>
2022-08-29 16:03:40 +0300

    update

---
+++ src/components/Map/Map.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'
+import { MyLocationOutlined } from '@material-ui/icons'
+import Rating from '@material-ui/lab'
+
+import useStyles from './style'
+
+const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width: 600px)');
+
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{key: 'AIzaSyAjokHSaJ5fJzqdJwBa9A8ihycBeTgXYSg'}}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        // onChange={''}
+        // onChildClick={''}
+      >
+
+      </GoogleMapReact>
+    </div>
+  )
+}
+
+export default Map
